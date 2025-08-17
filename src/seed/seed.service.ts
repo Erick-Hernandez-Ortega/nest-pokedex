4566b841fb2add597de1a4629f25b1fc159693ae
Erick Ramon Hernandez Ortega <erickramon47@live.com.mx>
2025-08-16 18:18:43 -0600

    feat: create seed endpoint

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-interfaces.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
+  private readonly axios: AxiosInstance = axios;
 
-  
-  executeSeed() {
-    return 'execute seed'
+  constructor(
+    @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>
+  ){}
+
+  async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    const insertPokemon: {name: string; no: number}[] = [];
+
+    data.results.forEach(({name, url}) => {
+      const segments = url.split('/');
+      const no: number = Number(segments[segments.length - 2]);
+      
+      insertPokemon.push({name, no});
+    });
+
+    await this.pokemonModel.insertMany(insertPokemon);
+
+    return {
+      message: 'Seed execute!'
+    };
   }
 }
